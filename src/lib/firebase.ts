1ea37e71856a3f2440a8d85c2613c5ebd958eb14
Satish Mane <scm098@gmail.com>
2026-07-15 07:18:31 +0000

    How to Fix This in Your Code You need to update your Firebase config ini

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCMhxzeh_qZM8MC8_3Nq_hPbmwuqIwyCjY",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "abacusace-mmnqw.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "abacusace-mmnqw",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "abacusace-mmnqw.appspot.com",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "abacusace-mmnqw.firebasestorage.app",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "937539932258",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:937539932258:web:55fe45cb88a03298e1013b",
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
